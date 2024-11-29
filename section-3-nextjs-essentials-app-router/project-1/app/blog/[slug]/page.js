50a68155862717ae992ee42e0f4d85ec2f5533cb
suryathink <suryaprakashsingh110@gmail.com>
2024-11-29 10:33:31 +0530

    init

---
+++ section-3-nextjs-essentials-app-router/project-1/app/blog/[slug]/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <main>
+      <h1>The Blog</h1>
+      <p><Link href=></Link></p>
+      <p><Link></Link></p>
+    </main>
+  );
+};
+
+export default BlogPage;
